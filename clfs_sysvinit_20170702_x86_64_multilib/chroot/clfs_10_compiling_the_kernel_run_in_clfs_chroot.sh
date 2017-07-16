42e82522adbfb46813e7b703b00e548e28370b17
overflyer <puetz-thomas@gmx.net>
2017-07-16 23:49:38 +0200

    Update clfs_10_compiling_the_kernel_run_in_clfs_chroot.sh

--- clfs_sysvinit_20170702_x86_64_multilib/chroot/clfs_10_compiling_the_kernel_run_in_clfs_chroot.sh
+++ clfs_sysvinit_20170702_x86_64_multilib/chroot/clfs_10_compiling_the_kernel_run_in_clfs_chroot.sh
@@ -9,6 +9,38 @@
 
 #=====================
 
+#Building the final CLFS System
+CLFS=/
+CLFSHOME=/home
+CLFSSOURCES=/sources
+CLFSTOOLS=/tools
+CLFSCROSSTOOLS=/cross-tools
+CLFSFILESYSTEM=ext4
+CLFSROOTDEV=/dev/sda4
+CLFSHOMEDEV=/dev/sda5
+MAKEFLAGS='j8'
+BUILD32="-m32"
+BUILD64="-m64"
+CLFS_TARGET32="i686-pc-linux-gnu"
+PKG_CONFIG_PATH32=/usr/lib/pkgconfig
+PKG_CONFIG_PATH64=/usr/lib64/pkgconfig
+
+export CLFS=/
+export CLFSUSER=clfs
+export CLFSHOME=/home
+export CLFSSOURCES=/sources
+export CLFSTOOLS=/tools
+export CLFSCROSSTOOLS=/cross-tools
+export CLFSFILESYSTEM=ext4
+export CLFSROOTDEV=/dev/sda4
+export CLFSHOMEDEV=/dev/sda5
+export MAKEFLAGS=j8
+export BUILD32="-m32"
+export BUILD64="-m64"
+export CLFS_TARGET32="i686-pc-linux-gnu"
+export PKG_CONFIG_PATH32=/usr/lib/pkgconfig
+export PKG_CONFIG_PATH64=/usr/lib64/pkgconfig
+
 cd ${CLFSSOURCES}
 
 mkdir /etc/modprobe.d
