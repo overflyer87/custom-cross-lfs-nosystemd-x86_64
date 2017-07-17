b6d4cc0809bf75b2277f0d8a4225188f9d5e1cc7
overflyer <puetz-thomas@gmx.net>
2017-07-17 12:47:29 +0200

    Update clfs_purge_after_virtual_kfs_mounted.sh

--- clfs_sysvinit_20170702_x86_64_multilib/chroot/purge/clfs_purge_after_virtual_kfs_mounted.sh
+++ clfs_sysvinit_20170702_x86_64_multilib/chroot/purge/clfs_purge_after_virtual_kfs_mounted.sh
@@ -1,1 +1,23 @@
 
+#!/bin/bash
+
+#Reverse CLFS Installation until Temporary System
+
+CLFS=/mnt/clfs
+CLFSHOME=/mnt/clfs/home
+CLFSUSER="clfs"
+
+sudo -f umount ${CLFS}/dev/pts
+sudo -f umount ${CLFS}/dev
+sudo -f umount ${CLFS}/home
+sudo -f umount ${CLFS}/sys/firmware/efi/efivars
+sudo -f umount ${CLFS}/sys
+sudo -f umount ${CLFS}/run
+sudo -f umount ${CLFS}/proc
+sudo userdel ${CLFSUSER}
+sudo groupdel ${CLFSUSER}
+sudo rm -r /home/${CLFSUSER}
+sudo rm -rf ${CLFS}/*
+sudo umount -f ${CLFS}
+sudo unlink /cross-tools 
+sudo unlink /tools
