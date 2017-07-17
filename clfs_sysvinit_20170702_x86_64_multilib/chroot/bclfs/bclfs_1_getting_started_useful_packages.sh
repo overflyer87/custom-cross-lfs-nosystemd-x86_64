a828c48ea5cf5f8ae384d25ee485ca550f8908e3
overflyer <puetz-thomas@gmx.net>
2017-07-17 21:28:55 +0200

    Update bclfs_1_getting_started_useful_packages.sh

--- clfs_sysvinit_20170702_x86_64_multilib/chroot/bclfs/bclfs_1_getting_started_useful_packages.sh
+++ clfs_sysvinit_20170702_x86_64_multilib/chroot/bclfs/bclfs_1_getting_started_useful_packages.sh
@@ -160,6 +160,36 @@ rm -rf git
 mkdir openssh && tar xf openssh-*.tat.* -C openssh --strip-components 1
 cd openssh
 
+install  -v -m700 -d /var/lib/sshd &&
+chown    -v root:sys /var/lib/sshd &&
+
+groupadd -g 50 sshd        &&
+useradd  -c 'sshd PrivSep' \
+         -d /var/lib/sshd  \
+         -g sshd           \
+         -s /bin/false     \
+         -u 50 sshd
+
+patch -Np1 -i ../openssh-7.5p1-openssl-1.1.0-1.patch &&
+
+CC="gcc ${BUILD64}" USE_ARCH=64 PKG_CONFIG_PATH="${PKG_CONFIG_PATH64}" \
+CXX="g++ ${BUILD64}" \
+./configure --prefix=/usr                     \
+            --sysconfdir=/etc/ssh             \
+            --libdir=/usr/lib64               \
+            --with-md5-passwords              \
+            --with-privsep-path=/var/lib/sshd &&
+
+PREFIX=/usr LIBDIR=/usr/lib64 make
+PREFIX=/usr LIBDIR=/usr/lib64 make install
+
+install -v -m755    contrib/ssh-copy-id /usr/bin     &&
+
+install -v -m644    contrib/ssh-copy-id.1 \
+                    /usr/share/man/man1              &&
+install -v -m755 -d /usr/share/doc/openssh-7.5p1     &&
+install -v -m644    INSTALL LICENCE OVERVIEW README* \
+                    /usr/share/doc/openssh-7.5p1
 
 
 cd ${CLFSSOURCES}
