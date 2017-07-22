975525d1fa7b11ad6cc24a514fa29d6a4f0627ee
overflyer <puetz-thomas@gmx.net>
2017-07-22 18:07:23 +0200

    Update bclfs_3_experimental_openRC.sh

--- clfs_sysvinit_20170702_x86_64_multilib/chroot/bclfs/bclfs_3_experimental_openRC.sh
+++ clfs_sysvinit_20170702_x86_64_multilib/chroot/bclfs/bclfs_3_experimental_openRC.sh
@@ -91,7 +91,7 @@ cd openrc
 sed -e "s|/sbin|/usr/bin|g" -i support/sysvinit/inittab
 sed -i 's:0444:0644:' mk/sys.mk
 
-PKG_CONFIG_PATH=${PKG_CONFIG_PATH64}/ \
+PKG_CONFIG_PATH=${PKG_CONFIG_PATH64} \
 BRANDING='CLFS-20170702-x86_64-multilib' \ 
 #MKPAM=pam \
 MKSELINUX=no \
