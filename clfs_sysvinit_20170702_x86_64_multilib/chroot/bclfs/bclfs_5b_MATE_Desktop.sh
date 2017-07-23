9abd49ac493c4f4108f0083f786ea32b31762fd5
overflyer <puetz-thomas@gmx.net>
2017-07-23 21:11:20 +0200

    Update bclfs_5b_MATE_Desktop.sh

--- clfs_sysvinit_20170702_x86_64_multilib/chroot/bclfs/bclfs_5b_MATE_Desktop.sh
+++ clfs_sysvinit_20170702_x86_64_multilib/chroot/bclfs/bclfs_5b_MATE_Desktop.sh
@@ -92,3 +92,110 @@ export PKG_CONFIG_PATH="${PKG_CONFIG_PATH64}"
 export USE_ARCH=64 
 export CXX="g++ ${BUILD64}" 
 export CC="gcc ${BUILD64}"
+
+#We left off installing gtk3
+#Now we continue with libxslt which first needs libxml2 (WITH THE PYTHON MODULE!!!)
+
+#libxml2 WITH ITS PYTHON MODULE
+wget http://xmlsoft.org/sources/libxml2-2.9.4.tar.gz -O \
+    libxml2-2.9.4.tar.gz
+
+#Download testsuite. WE NEED IT to build the Python module!
+wget http://www.w3.org/XML/Test/xmlts20130923.tar.gz -O \
+    xmlts20130923.tar.gz
+
+mkdir libxml2 && tar xf libxml2-*.tar.* -C libxml2 --strip-components 1
+cd libxml2
+
+#run this to build Pytghon3 module
+#Python2 module would be the default
+#We try not to use Python2 in CLFS multib!
+sed -i '/_PyVerify_fd/,+1d' python/types.c
+
+PKG_CONFIG_PATH="${PKG_CONFIG_PATH64}" ./configure --prefix=/usr \
+   --disable-static \
+   --with-history   \
+   --libdir=/usr/lib64 \
+   --with-python=/usr/bin/python3 \
+   --with-icu \
+   --with-threads
+
+PKG_CONFIG_PATH=${PKG_CONFIG_PATH64} make PREFIX=/usr LIBDIR=/usr/lib64
+
+tar xf ../xmlts20130923.tar.gz
+make check > check.log
+grep -E '^Total|expected' check.log
+checkBuiltPackage
+
+as_root make PREFIX=/usr LIBDIR=/usr/lib64 install 
+
+cd ${CLFSSOURCES}/xc/mate
+as_root updatedb
+as_root locate libxml | grep /usr/lib64/python3.6/
+echo "Did locate libxml | grep /usr/lib64/python3.6/ find the libxml python modules?"
+echo ""
+checkBuiltPackage
+checkBuiltPackage
+rm -rf libxml2
+
+#libxslt
+wget http://xmlsoft.org/sources/libxslt-1.1.29.tar.gz -O \
+    libxslt-1.1.29.tar.gz 
+
+mkdir libxslt && tar xf libxslt-*.tar.* -C libxslt --strip-components 1
+cd libxslt
+
+PKG_CONFIG_PATH="${PKG_CONFIG_PATH64}" ./configure --prefix=/usr \
+   --disable-static \
+   --libdir=/usr/lib64 \
+
+PKG_CONFIG_PATH="${PKG_CONFIG_PATH64}" make PREFIX=/usr LIBDIR=/usr/lib64
+as_root make PREFIX=/usr LIBDIR=/usr/lib64 install
+
+ldconfig
+
+cd ${CLFSSOURCES}/xc/mate
+checkBuiltPackage
+rm -rf libxslt
+
+#Before dconf we need to build everything needed for GTK-Doc
+#That is when you have no internet connection!!!
+#Otherwise disable nonet parameter for xsltproc in doc/Makefile
+#BLFS does not mention that
+#Otherwise dconf fails with 
+#I/O error : Attempt to load network entity http://docbook.sourceforge.net/release/xsl/current/manpages/docbook.xsl
+
+#dconf and dconf-editor
+wget http://ftp.gnome.org/pub/gnome/sources/dconf/0.26/dconf-0.26.0.tar.xz -O \
+    Dconf-0.26.0.tar.xz
+
+wget http://ftp.gnome.org/pub/gnome/sources/dconf-editor/3.22/dconf-editor-3.22.3.tar.xz -O \
+    dconf-editor-3.22.3.tar.xz
+
+mkdir dconf && tar xf Dconf-*.tar.* -C dconf --strip-components 1
+cd dconf
+
+#This 'patch' only works when you have a working itnernet connection
+sed -i 's/--nonet//' docs/Makefile
+
+PKG_CONFIG_PATH="${PKG_CONFIG_PATH64}" ./configure --prefix=/usr \
+   --libdir=/usr/lib64 \
+   --sysconfdir=/etc \
+   --disable-gtk-doc
+
+PKG_CONFIG_PATH="${PKG_CONFIG_PATH64}" make PREFIX=/usr LIBDIR=/usr/lib64
+as_root make PREFIX=/usr LIBDIR=/usr/lib64 install
+
+tar -xf ../dconf-editor-3.22.3.tar.xz &&
+cd dconf-editor-3.22.3 &&
+
+PKG_CONFIG_PATH="${PKG_CONFIG_PATH64}" ./configure --prefix=/usr \
+   --libdir=/usr/lib64 \
+   --sysconfdir=/etc
+
+PKG_CONFIG_PATH="${PKG_CONFIG_PATH64}" make PREFIX=/usr LIBDIR=/usr/lib64
+as_root make PREFIX=/usr LIBDIR=/usr/lib64 install
+
+cd ${CLFSSOURCES}/xc/mate
+checkBuiltPackage
+rm -rf dconf
