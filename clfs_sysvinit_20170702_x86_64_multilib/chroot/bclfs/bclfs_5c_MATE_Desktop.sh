778e43780610501f84d69d8be4543a526edfbf62
overflyer <puetz-thomas@gmx.net>
2017-07-24 00:08:52 +0200

    Create bclfs_5c_MATE_Desktop.sh

---
+++ clfs_sysvinit_20170702_x86_64_multilib/chroot/bclfs/bclfs_5c_MATE_Desktop.sh
@@ -0,0 +1,112 @@
+#!/bin/bash
+
+function checkBuiltPackage() {
+
+echo "Did everything build fine?: [Y/N]"
+while read -n1 -r -p "[Y/N]   " && [[ $REPLY != q ]]; do
+  case $REPLY in
+    Y) break 1;;
+    N) echo "$EXIT"
+       echo "Fix it!"
+       exit 1;;
+    *) echo " Try again. Type y or n";;
+  esac
+done
+
+}
+
+function as_root()
+{
+  if   [ $EUID = 0 ];        then $*
+  elif [ -x /usr/bin/sudo ]; then sudo $*
+  else                            su -c \\"$*\\"
+  fi
+}
+
+export -f as_root
+
+function buildSingleXLib32() {
+  PKG_CONFIG_PATH="${PKG_CONFIG_PATH32}" \
+  USE_ARCH=32 CC="gcc ${BUILD32}" CXX="g++ ${BUILD32}" ./configure $XORG_CONFIG32
+  make PREFIX=/usr LIBDIR=/usr/lib
+  as_root make PREFIX=/usr LIBDIR=/usr/lib install
+}
+
+export -f buildSingleXLib32
+
+function buildSingleXLib64() {
+  PKG_CONFIG_PATH="${PKG_CONFIG_PATH64}" \
+  USE_ARCH=64 CC="gcc ${BUILD64}" CXX="g++ ${BUILD64}" ./configure $XORG_CONFIG64
+  make PREFIX=/usr LIBDIR=/usr/lib64
+  as_root make PREFIX=/usr LIBDIR=/usr/lib64 install
+}
+
+
+export -f buildSingleXLib64
+
+#Building the final CLFS System
+CLFS=/
+CLFSHOME=/home
+CLFSSOURCES=/sources
+CLFSTOOLS=/tools
+CLFSCROSSTOOLS=/cross-tools
+CLFSFILESYSTEM=ext4
+CLFSROOTDEV=/dev/sda4
+CLFSHOMEDEV=/dev/sda5
+MAKEFLAGS='j8'
+BUILD32="-m32"
+BUILD64="-m64"
+CLFS_TARGET32="i686-pc-linux-gnu"
+PKG_CONFIG_PATH32=/usr/lib/pkgconfig
+PKG_CONFIG_PATH64=/usr/lib64/pkgconfig
+
+export CLFS=/
+export CLFSUSER=clfs
+export CLFSHOME=/home
+export CLFSSOURCES=/sources
+export CLFSTOOLS=/tools
+export CLFSCROSSTOOLS=/cross-tools
+export CLFSFILESYSTEM=ext4
+export CLFSROOTDEV=/dev/sda4
+export CLFSHOMEDEV=/dev/sda5
+export MAKEFLAGS=j8
+export BUILD32="-m32"
+export BUILD64="-m64"
+export CLFS_TARGET32="i686-pc-linux-gnu"
+export PKG_CONFIG_PATH32=/usr/lib/pkgconfig
+export PKG_CONFIG_PATH64=/usr/lib64/pkgconfig
+
+cd ${CLFSSOURCES}
+cd ${CLFSSOURCES}/xc/mate
+
+#We will only do 64-bit builds in this script
+#We compiled Xorg with 32-bit libraries
+#THat should suffice
+
+PKG_CONFIG_PATH="${PKG_CONFIG_PATH64}" 
+USE_ARCH=64 
+CXX="g++ ${BUILD64}" 
+CC="gcc ${BUILD64}"
+
+export PKG_CONFIG_PATH="${PKG_CONFIG_PATH64}" 
+export USE_ARCH=64 
+export CXX="g++ ${BUILD64}" 
+export CC="gcc ${BUILD64}"
+
+#We left off installing mate-desktop
+#Now we continue ligpgerror
+
+#libgpg-error
+wget ftp://ftp.gnupg.org/gcrypt/libgpg-error/libgpg-error-1.27.tar.bz2 -O \
+    libgpg-error-1.27.tar.bz2
+    
+mkdir libgpgerror && tar xf libgpg-error-*.tar.* -C libgpgerror --strip-components 1
+cd libgpgerror
+
+PKG_CONFIG_PATH="${PKG_CONFIG_PATH64}" ./configure --prefix=/usr --libdir=/usr/lib64
+PKG_CONFIG_PATH="${PKG_CONFIG_PATH64}"  make LIBDIR=/usr/lib64 PREFIX=/usr
+as_root LIBDIR=/usr/lib64 PREFIX=/usr install
+
+cd ${CLFSSOURCES}/xc/mate
+checkBuiltPackage
+rm -r libgpgerror
