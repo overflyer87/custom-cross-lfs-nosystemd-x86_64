830b3b8bbecdabe20d205b74a82290456ee84801
overflyer <puetz-thomas@gmx.net>
2017-07-23 15:10:40 +0200

    Update bclfs_4b_Xorg.sh

--- clfs_sysvinit_20170702_x86_64_multilib/chroot/bclfs/bclfs_4b_Xorg.sh
+++ clfs_sysvinit_20170702_x86_64_multilib/chroot/bclfs/bclfs_4b_Xorg.sh
@@ -77,45 +77,6 @@ export PKG_CONFIG_PATH32=/usr/lib/pkgconfig
 export PKG_CONFIG_PATH64=/usr/lib64/pkgconfig
 export ACLOCAL="aclocal -I $XORG_PREFIX/share/aclocal"
 
-cd ${CLFSSOURCES/xc}
-
-cat > lib-7.md5 << "EOF"
-c5ba432dd1514d858053ffe9f4737dd8  xtrans-1.3.5.tar.bz2
-0f618db70c4054ca67cee0cc156a4255  libX11-1.6.5.tar.bz2
-52df7c4c1f0badd9f82ab124fb32eb97  libXext-1.3.3.tar.bz2
-d79d9fe2aa55eb0f69b1a4351e1368f7  libFS-1.0.7.tar.bz2
-addfb1e897ca8079531669c7c7711726  libICE-1.0.9.tar.bz2
-499a7773c65aba513609fe651853c5f3  libSM-1.2.2.tar.bz2
-7a773b16165e39e938650bcc9027c1d5  libXScrnSaver-1.2.2.tar.bz2
-8f5b5576fbabba29a05f3ca2226f74d3  libXt-1.1.5.tar.bz2
-41d92ab627dfa06568076043f3e089e4  libXmu-1.1.2.tar.bz2
-20f4627672edb2bd06a749f11aa97302  libXpm-3.5.12.tar.bz2
-e5e06eb14a608b58746bdd1c0bd7b8e3  libXaw-1.0.13.tar.bz2
-07e01e046a0215574f36a3aacb148be0  libXfixes-5.0.3.tar.bz2
-f7a218dcbf6f0848599c6c36fc65c51a  libXcomposite-0.4.4.tar.bz2
-802179a76bded0b658f4e9ec5e1830a4  libXrender-0.9.10.tar.bz2
-1e7c17afbbce83e2215917047c57d1b3  libXcursor-1.1.14.tar.bz2
-0cf292de2a9fa2e9a939aefde68fd34f  libXdamage-1.1.4.tar.bz2
-0920924c3a9ebc1265517bdd2f9fde50  libfontenc-1.1.3.tar.bz2
-0d9f6dd9c23bf4bcbfb00504b566baf5  libXfont2-2.0.1.tar.bz2
-331b3a2a3a1a78b5b44cfbd43f86fcfe  libXft-2.3.2.tar.bz2
-1f0f2719c020655a60aee334ddd26d67  libXi-1.7.9.tar.bz2
-9336dc46ae3bf5f81c247f7131461efd  libXinerama-1.1.3.tar.bz2
-28e486f1d491b757173dd85ba34ee884  libXrandr-1.5.1.tar.bz2
-45ef29206a6b58254c81bea28ec6c95f  libXres-1.0.7.tar.bz2
-ef8c2c1d16a00bd95b9fdcef63b8a2ca  libXtst-1.2.3.tar.bz2
-210b6ef30dda2256d54763136faa37b9  libXv-1.0.11.tar.bz2
-4cbe1c1def7a5e1b0ed5fce8e512f4c6  libXvMC-1.0.10.tar.bz2
-d7dd9b9df336b7dd4028b6b56542ff2c  libXxf86dga-1.1.4.tar.bz2
-298b8fff82df17304dfdb5fe4066fe3a  libXxf86vm-1.1.4.tar.bz2
-ba983eba5a9f05d152a0725b8e863151  libdmx-1.1.3.tar.bz2
-d810ab17e24c1418dedf7207fb2841d4  libpciaccess-0.13.5.tar.bz2
-4a4cfeaf24dab1b991903455d6d7d404  libxkbfile-1.0.9.tar.bz2
-66662e76899112c0f99e22f2fc775a7e  libxshmfence-1.2.tar.bz2
-EOF
-
-cd ${CLFSSOURCES}/xc
-
 mkdir lib
 cd lib
 grep -v '^#' ../lib-7.md5 | awk '{print $2}' | wget -i- -c \
