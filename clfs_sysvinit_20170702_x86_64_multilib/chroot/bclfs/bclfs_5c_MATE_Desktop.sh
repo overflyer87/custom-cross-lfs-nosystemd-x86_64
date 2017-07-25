7112a73494332d2343ac18efdc124dbfda17ff48
overflyer <puetz-thomas@gmx.net>
2017-07-25 19:47:38 +0200

    Update bclfs_5c_MATE_Desktop.sh

--- clfs_sysvinit_20170702_x86_64_multilib/chroot/bclfs/bclfs_5c_MATE_Desktop.sh
+++ clfs_sysvinit_20170702_x86_64_multilib/chroot/bclfs/bclfs_5c_MATE_Desktop.sh
@@ -298,14 +298,15 @@ PKG_CONFIG_PATH="${PKG_CONFIG_PATH64}" ./configure --prefix=/usr \
 #with the hardcoded path of anything containing html/docbook.xsl on you system
 #It is most likely to be found somewhere in /usr/share/xml ...
 
-sed -i 's/XSLTPROC_XSL = \\//' Makefile
-sed -i 's/http\:\/\/docbook.sourceforge.net\/release\/xsl\/current\/manpages\/docbook.xsl//' Makefile
-sed -i '2750i XSLTPROC_XSL = \\  /usr/share/xml/docbook/xsl-stylesheets-1.79.1/html/docbook.xsl' Makefile
+export XSLTPROC_XSL=/usr/share/xml/docbook/xsl-stylesheets-1.79.1/html/docbook.xsl
 
-sed -i 's/http\:\/\/docbook.sourceforge.net\/release\/xsl\/current\/manpages\/docbook.xsl/ \
-\/usr\/share\/xml\/docbook\/xsl-stylesheets-1.79.1\/html\/docbook.xsl/' Makefile.in docs/Makefile.am
+sed -i 's/XSLTPROC_XSL = \\//' Makefile docs/Makefile.am
+sed -i 's/http\:\/\/docbook.sourceforge.net\/release\/xsl\/current\/manpages\/docbook.xsl//' Makefile docs/Makefile.am
+#sed -i '2750i XSLTPROC_XSL = \\  /usr/share/xml/docbook/xsl-stylesheets-1.79.1/html/docbook.xsl' Makefile docs/Makefile.am
+#sed -i 's/http\:\/\/docbook.sourceforge.net\/release\/xsl\/current\/manpages\/docbook.xsl/ \
+#\/usr\/share\/xml\/docbook\/xsl-stylesheets-1.79.1\/html\/docbook.xsl/' Makefile.in docs/Makefile.am
 
-
+XSLTPROC_XSL=/usr/share/xml/docbook/xsl-stylesheets-1.79.1/html/docbook.xsl \
 PKG_CONFIG_PATH="${PKG_CONFIG_PATH64}" make LIBDIR=/usr/lib64 PREFIX=/usr
 
 make check
