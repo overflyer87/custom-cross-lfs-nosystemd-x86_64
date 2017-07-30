00e34a88cfde3b5b7d96acf527064da3765f1e28
overflyer <puetz-thomas@gmx.net>
2017-07-30 19:45:38 +0200

    Update bclfs_5c_MATE_Desktop_x64.sh

--- clfs_sysvinit_20170702_x86_64_multilib/chroot/bclfs/bclfs_5c_MATE_Desktop_x64.sh
+++ clfs_sysvinit_20170702_x86_64_multilib/chroot/bclfs/bclfs_5c_MATE_Desktop_x64.sh
@@ -557,7 +557,7 @@ checkBuiltPackage
 
 as_root make LIBDIR=/usr/lib64 PREFIX=/usr install
 as_root unlink /usr/bin/python
-as_root cp libsoup-2.4.pc ${PKG_CONFIG_PATH64}
+as_root ldconfig
 
 cd ${CLFSSOURCES}/xc/mate
 checkBuiltPackage
@@ -570,13 +570,12 @@ wget https://github.com/mate-desktop/libmateweather/archive/v1.19.1.tar.gz -O \
 mkdir libmateweather && tar xf libmateweather-*.tar.* -C libmateweather --strip-components 1
 cd libmateweather
 
- LIBSOUP_LIBS=/usr/lib64 \
   ACLOCAL_FLAG=/usr/share/aclocal/ CC="gcc ${BUILD64}" \
   CXX="g++ ${BUILD64}" USE_ARCH=64 \
    PKG_CONFIG_PATH=${PKG_CONFIG_PATH64} sh autogen.sh --prefix=/usr \
    --libdir=/usr/lib64 --sysconfdir=/etc --disable-static \
    --localstatedir=/var --bindir=/usr/bin --sbindir=/usr/sbin \
-   --disable-docbook-docs
+   --enable-dependency-tracking
 
 PKG_CONFIG_PATH="${PKG_CONFIG_PATH64}" make LIBDIR=/usr/lib64 PREFIX=/usr
 as_root make LIBDIR=/usr/lib64 PREFIX=/usr install
