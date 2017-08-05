d9d5c48397a6b0a1e4c41035d8b510fc39da9424
overflyer <puetz-thomas@gmx.net>
2017-08-05 16:48:47 +0200

    Update clfs_9_final_sys_conf_run_in_clfs_chroot.sh

--- clfs_sysvinit_20170702_x86_64_multilib/chroot/clfs_9_final_sys_conf_run_in_clfs_chroot.sh
+++ clfs_sysvinit_20170702_x86_64_multilib/chroot/clfs_9_final_sys_conf_run_in_clfs_chroot.sh
@@ -76,165 +76,6 @@ SUBSYSTEM=="block", ENV{ID_TYPE}=="cd", \
 
 EOF
 
-cat > /etc/profile << "EOF"
-# Begin /etc/profile
-
-for f in /etc/bash_completion.d/*
-do
-  if [ -e ${f} ]; then source ${f}; fi
-done
-unset f
-
-export INPUTRC=/etc/inputrc
-EOF
-
-cat > /etc/profile << "EOF"
-# Begin /etc/profile
-
-for script in /etc/profile.d/*.sh
-do
-  source $script
-done
-unset script
-
-# End /etc/profile
-EOF
-
-install -d -m755 /etc/profile.d
-
-cat > /etc/profile.d/05-i18n.sh << "EOF"
-# Begin /etc/profile.d/05-i18n.sh
-
-export LANG=[ll]_[CC].[charset]
-export G_FILENAME_ENCODING=@locale
- 
-# End /etc/profile.d/05-i18n.sh
-EOF
-
-cat > /etc/profile.d/10-path.sh << "EOF"
-# Begin /etc/profile.d/10-path.sh
-
-if [ "$EUID" -eq 0 ]; then
-  export PATH="/sbin:/bin:/usr/sbin:/usr/bin"
-  if [ -d "/usr/local/sbin" ]; then
-    export PATH="$PATH:/usr/local/sbin"
-  fi
-else
-  export PATH="/bin:/usr/bin"
-fi
-
-if [ -d "/usr/local/bin" ]; then
-  export PATH="$PATH:/usr/local/bin"
-fi
-
-if [ -d "$HOME/bin" ]; then
-  export PATH="$HOME/bin:$PATH"
-fi
-
-# End /etc/profile.d/10-path.sh
-EOF
-
-cat > /etc/profile.d/10-pkg_config_path.sh << "EOF"
-# Begin /etc/profile.d/10-pkg_config_path.sh
-
-export PKG_CONFIG_PATH32="/usr/lib/pkgconfig"
-export PKG_CONFIG_PATHN32="/usr/lib32/pkgconfig"
-export PKG_CONFIG_PATH64="/usr/lib64/pkgconfig"
-
-# End /etc/profile.d/10-pkg_config_path.sh
-EOF
-
-cat > /etc/profile.d/10-xdg.sh << "EOF"
-# Begin /etc/profild.d/10-xdg.sh
-
-export XDG_DATA_DIRS="/usr/share"
-export XDG_CONFIG_DIRS="/etc/xdg:/usr/share"
-
-# End /etc/profild.d/10-xdg.sh
-EOF
-
-cat > /etc/profile.d/50-dircolors.sh << "EOF"
-# Begin /etc/profile.d/50-dircolors.sh
-
-alias ls='ls --color=auto'
-if [ -f "$HOME/.dircolors" ]; then
-  eval `dircolors -b "$HOME/.dircolors"`
-else
-  if [ -f "/etc/dircolors" ]; then
-    eval `dircolors -b "/etc/dircolors"`
-  fi
-fi
-
-# End /etc/profile.d/50-dircolors.sh
-EOF
-
-dircolors -p > /etc/dircolors
-
-cat > /etc/profile.d/50-history.sh << "EOF"
-# Begin /etc/profile.d/50-history.sh
-
-export HISTSIZE=1000
-export HISTIGNORE="&:[bf]g:exit"
-
-# End /etc/profile.d/50-history.sh
-EOF
-
-cat > /etc/profile.d/50-prompt.sh << "EOF"
-# Begin /etc/profile.d/50-prompt.sh
-
-export PS1="\u:\w\$ "
-if [ "${TERM:0:5}" = "xterm" ]; then
-  export PS1="\[\e]2;\u@\H :: \w\a\]$PS1"
-fi
-
-shopt -s checkwinsize
-
-# End /etc/profile.d/50-prompt.sh
-EOF
-
-cat > /etc/profile.d/50-readline.sh << "EOF"
-# Begin /etc/profile.d/50-readline.sh
-
-if [ -z "$INPUTRC" ]; then
-  if [ -f "$HOME/.inputrc" ]; then
-    export INPUTRC="$HOME/.inputrc"
-  else
-    if [ -f "/etc/inputrc" ]; then
-      export INPUTRC="/etc/inputrc"
-    fi
-  fi
-fi
-
-# End /etc/profile.d/50-readline.sh
-EOF
-
-cat > /etc/profile.d/50-umask.sh << "EOF"
-# Begin /etc/profile.d/50-umask.sh
-
-if [ "`id -un`" = "`id -gn`" -a $EUID -gt 99 ]; then
-  umask 002
-else
-  umask 022
-fi
-
-# End /etc/profile.d/50-umask.sh
-EOF
-
-cat > /etc/profile.d/50-multilib.sh << "EOF"
-# Begin /etc/profile.d/50-multilib.sh
-
-export BUILD32="-m32"
-export BUILD64="-m64"
-
-export CLFS_TARGET32="i686-pc-linux-gnu"
-
-export LD_BUILD32="-m elf_i386"
-export LD_BUILD64="-m elf_x86_64"
-
-# End /etc/profile.d/50-multilib.sh
-EOF
-
-
 cat >> /etc/locale.conf << "EOF"
 
 LC_ALL=de_DE.utf8 locale territory 
@@ -252,52 +93,6 @@ cat >> /etc/profile << "EOF"
 export LANG=en_US.utf8
 
 # End /etc/profile
-EOF
-
-cat > /etc/inputrc << "EOF"
-# Begin /etc/inputrc
-# Modified by Chris Lynn <roryo@roryo.dynup.net>
-
-# Allow the command prompt to wrap to the next line
-set horizontal-scroll-mode Off
-
-# Enable 8bit input
-set meta-flag On
-set input-meta On
-
-# Turns off 8th bit stripping
-set convert-meta Off
-
-# Keep the 8th bit for display
-set output-meta On
-
-# none, visible or audible
-set bell-style none
-
-# All of the following map the escape sequence of the
-# value contained inside the 1st argument to the
-# readline specific functions
-
-"\eOd": backward-word
-"\eOc": forward-word
-
-# for linux console
-"\e[1~": beginning-of-line
-"\e[4~": end-of-line
-"\e[5~": beginning-of-history
-"\e[6~": end-of-history
-"\e[3~": delete-char
-"\e[2~": quoted-insert
-
-# for xterm
-"\eOH": beginning-of-line
-"\eOF": end-of-line
-
-# for Konsole
-"\e[H": beginning-of-line
-"\e[F": end-of-line
-
-# End /etc/inputrc
 EOF
 
 cat > /etc/fstab << "EOF"
@@ -403,10 +198,294 @@ DISTRIB_CODENAME="overflyer"
 DISTRIB_DESCRIPTION="Cross Linux From Scratch"
 EOF
 
-
 cd ${CLFSSOURCES} 
 checkBuiltPackage
 rm -rf lsbrel
+
+#Bash startup files according to BLFS
+#And one multilib sh from CBLFS
+
+install --directory --mode=0755 --owner=root --group=root /etc/profile.d
+
+cat > /etc/profile.d/50-multilib.sh << "EOF"
+# Begin /etc/profile.d/50-multilib.sh
+
+export BUILD32="-m32"
+export BUILD64="-m64"
+
+export CLFS_TARGET32="i686-pc-linux-gnu"
+
+export LD_BUILD32="-m elf_i386"
+export LD_BUILD64="-m elf_x86_64"
+
+# End /etc/profile.d/50-multilib.sh
+EOF
+
+#Now only BLFS scripts follow
+cat > /etc/profile << "EOF"
+# Begin /etc/profile
+# Written for Beyond Linux From Scratch
+# by James Robertson <jameswrobertson@earthlink.net>
+# modifications by Dagmar d'Surreal <rivyqntzne@pbzpnfg.arg>
+
+# System wide environment variables and startup programs.
+
+# System wide aliases and functions should go in /etc/bashrc.  Personal
+# environment variables and startup programs should go into
+# ~/.bash_profile.  Personal aliases and functions should go into
+# ~/.bashrc.
+
+# Functions to help us manage paths.  Second argument is the name of the
+# path variable to be modified (default: PATH)
+pathremove () {
+        local IFS=':'
+        local NEWPATH
+        local DIR
+        local PATHVARIABLE=${2:-PATH}
+        for DIR in ${!PATHVARIABLE} ; do
+                if [ "$DIR" != "$1" ] ; then
+                  NEWPATH=${NEWPATH:+$NEWPATH:}$DIR
+                fi
+        done
+        export $PATHVARIABLE="$NEWPATH"
+}
+
+pathprepend () {
+        pathremove $1 $2
+        local PATHVARIABLE=${2:-PATH}
+        export $PATHVARIABLE="$1${!PATHVARIABLE:+:${!PATHVARIABLE}}"
+}
+
+pathappend () {
+        pathremove $1 $2
+        local PATHVARIABLE=${2:-PATH}
+        export $PATHVARIABLE="${!PATHVARIABLE:+${!PATHVARIABLE}:}$1"
+}
+
+export -f pathremove pathprepend pathappend
+
+# Set the initial path
+export PATH=/bin:/usr/bin
+
+if [ $EUID -eq 0 ] ; then
+        pathappend /sbin:/usr/sbin
+        unset HISTFILE
+fi
+
+# Setup some environment variables.
+export HISTSIZE=1000
+export HISTIGNORE="&:[bf]g:exit"
+
+# Set some defaults for graphical systems
+export XDG_DATA_DIRS=/usr/share/
+export XDG_CONFIG_DIRS=/etc/xdg/
+export XDG_RUNTIME_DIR=/tmp/xdg-$USER
+
+# Setup a red prompt for root and a green one for users.
+NORMAL="\[\e[0m\]"
+RED="\[\e[1;31m\]"
+GREEN="\[\e[1;32m\]"
+if [[ $EUID == 0 ]] ; then
+  PS1="$RED\u [ $NORMAL\w$RED ]# $NORMAL"
+else
+  PS1="$GREEN\u [ $NORMAL\w$GREEN ]\$ $NORMAL"
+fi
+
+for script in /etc/profile.d/*.sh ; do
+        if [ -r $script ] ; then
+                . $script
+        fi
+done
+
+unset script RED GREEN NORMAL
+
+# End /etc/profile
+EOF
+
+cat > /etc/profile.d/bash_completion.sh << "EOF"
+# Begin /etc/profile.d/bash_completion.sh
+# Import bash completion scripts
+
+for script in /etc/bash_completion.d/*.sh ; do
+        if [ -r $script ] ; then
+                . $script
+        fi
+done
+# End /etc/profile.d/bash_completion.sh
+EOF
+
+install --directory --mode=0755 --owner=root --group=root /etc/bash_completion.d
+
+cat > /etc/profile.d/dircolors.sh << "EOF"
+# Setup for /bin/ls and /bin/grep to support color, the alias is in /etc/bashrc.
+if [ -f "/etc/dircolors" ] ; then
+        eval $(dircolors -b /etc/dircolors)
+fi
+
+if [ -f "$HOME/.dircolors" ] ; then
+        eval $(dircolors -b $HOME/.dircolors)
+fi
+
+alias ls='ls --color=auto'
+alias grep='grep --color=auto'
+EOF
+
+cat > /etc/profile.d/extrapaths.sh << "EOF"
+if [ -d /usr/local/lib/pkgconfig ] ; then
+        pathappend /usr/local/lib/pkgconfig PKG_CONFIG_PATH
+fi
+if [ -d /usr/local/bin ]; then
+        pathprepend /usr/local/bin
+fi
+if [ -d /usr/local/sbin -a $EUID -eq 0 ]; then
+        pathprepend /usr/local/sbin
+fi
+
+# Set some defaults before other applications add to these paths.
+pathappend /usr/share/man  MANPATH
+pathappend /usr/share/info INFOPATH
+EOF
+
+cat > /etc/profile.d/readline.sh << "EOF"
+# Setup the INPUTRC environment variable.
+if [ -z "$INPUTRC" -a ! -f "$HOME/.inputrc" ] ; then
+        INPUTRC=/etc/inputrc
+fi
+export INPUTRC
+EOF
+
+cat > /etc/profile.d/umask.sh << "EOF"
+# By default, the umask should be set.
+if [ "$(id -gn)" = "$(id -un)" -a $EUID -gt 99 ] ; then
+  umask 002
+else
+  umask 022
+fi
+EOF
+
+cat > /etc/profile.d/i18n.sh << "EOF"
+# Set up i18n variables
+export LANG=<ll>_<CC>.<charmap><@modifiers>
+EOF
+
+cat > /etc/bashrc << "EOF"
+# Begin /etc/bashrc
+# Written for Beyond Linux From Scratch
+# by James Robertson <jameswrobertson@earthlink.net>
+# updated by Bruce Dubbs <bdubbs@linuxfromscratch.org>
+
+# System wide aliases and functions.
+
+# System wide environment variables and startup programs should go into
+# /etc/profile.  Personal environment variables and startup programs
+# should go into ~/.bash_profile.  Personal aliases and functions should
+# go into ~/.bashrc
+
+# Provides colored /bin/ls and /bin/grep commands.  Used in conjunction
+# with code in /etc/profile.
+
+alias ls='ls --color=auto'
+alias grep='grep --color=auto'
+
+# Provides prompt for non-login shells, specifically shells started
+# in the X environment. [Review the LFS archive thread titled
+# PS1 Environment Variable for a great case study behind this script
+# addendum.]
+
+NORMAL="\[\e[0m\]"
+RED="\[\e[1;31m\]"
+GREEN="\[\e[1;32m\]"
+if [[ $EUID == 0 ]] ; then
+  PS1="$RED\u [ $NORMAL\w$RED ]# $NORMAL"
+else
+  PS1="$GREEN\u [ $NORMAL\w$GREEN ]\$ $NORMAL"
+fi
+
+unset RED GREEN NORMAL
+
+# End /etc/bashrc
+EOF
+
+cat > ~/.bash_profile << "EOF"
+# Begin ~/.bash_profile
+# Written for Beyond Linux From Scratch
+# by James Robertson <jameswrobertson@earthlink.net>
+# updated by Bruce Dubbs <bdubbs@linuxfromscratch.org>
+
+# Personal environment variables and startup programs.
+
+# Personal aliases and functions should go in ~/.bashrc.  System wide
+# environment variables and startup programs are in /etc/profile.
+# System wide aliases and functions are in /etc/bashrc.
+
+if [ -f "$HOME/.bashrc" ] ; then
+  source $HOME/.bashrc
+fi
+
+if [ -d "$HOME/bin" ] ; then
+  pathprepend $HOME/bin
+fi
+
+# Having . in the PATH is dangerous
+#if [ $EUID -gt 99 ]; then
+#  pathappend .
+#fi
+
+# End ~/.bash_profile
+EOF
+
+cat > ~/.profile << "EOF"
+# Begin ~/.profile
+# Personal environment variables and startup programs.
+
+if [ -d "$HOME/bin" ] ; then
+  pathprepend $HOME/bin
+fi
+
+# Set up user specific i18n variables
+#export LANG=<ll>_<CC>.<charmap><@modifiers>
+
+# End ~/.profile
+EOF
+
+cat > ~/.bashrc << "EOF"
+# Begin ~/.bashrc
+# Written for Beyond Linux From Scratch
+# by James Robertson <jameswrobertson@earthlink.net>
+
+# Personal aliases and functions.
+
+# Personal environment variables and startup programs should go in
+# ~/.bash_profile.  System wide environment variables and startup
+# programs are in /etc/profile.  System wide aliases and functions are
+# in /etc/bashrc.
+
+if [ -f "/etc/bashrc" ] ; then
+  source /etc/bashrc
+fi
+
+# Set up user specific i18n variables
+#export LANG=<ll>_<CC>.<charmap><@modifiers>
+
+# End ~/.bashrc
+EOF
+
+cat > ~/.bash_logout << "EOF"
+# Begin ~/.bash_logout
+# Written for Beyond Linux From Scratch
+# by James Robertson <jameswrobertson@earthlink.net>
+
+# Personal items to perform on logout.
+
+# End ~/.bash_logout
+EOF
+
+dircolors -p > /etc/dircolors
+
+#For more tipps
+#on Bash shell scripts
+#http://www.caliban.org/bash/index.shtml
+
 
 echo " "
 
