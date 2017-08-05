634b5eb7612b608befb5e708f4a8f09db72e9894
overflyer <puetz-thomas@gmx.net>
2017-08-05 21:59:40 +0200

    Create ineedtopee.sh

---
+++ clfs_sysvinit_20170702_x86_64_multilib/chroot/meta/ineedtopee.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo " "
+echo "Execute this script if you want NO checks or tests whatsoever"
+echo "Execute this script inside the path where all the clfs_*.sh scripts are"
+echo " "
+
+sed -i 's/checkBuiltPackage/#checkBuiltPackage/' *clfs_.sh 
+sed -i 's/function #checkBuiltPackage/function checkBuiltPackage/' *clfs_.sh 
+sed -i 's/make check/#make check/' *clfs_.sh 
+sed -i 's/make -k check/#make -k check/' *clfs_.sh 
+sed -i 's/make test/#make test/' *clfs_.sh 
+sed -i 's/make -j1 test/#make -j1 test/' *clfs_.sh 
+sed -i 's/make tests/#make tests/'
+sed -i 's/make -j1 tests/#make -j1 tests/' *clfs_.sh 
+sed -i 's/make -k test/#make -k test/' *clfs_.sh 
+
+echo " "
+echo "All checks and tests are disabled"
+echo " "
