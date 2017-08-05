0189318fd1b0a3b1e22f0b907877f010fd6d3f0a
overflyer <puetz-thomas@gmx.net>
2017-08-05 22:57:35 +0200

    Update clfs_0b_login_as_clfs_user_run_as_host_root.sh

--- clfs_sysvinit_20170702_x86_64_multilib/chroot/clfs_0b_login_as_clfs_user_run_as_host_root.sh
+++ clfs_sysvinit_20170702_x86_64_multilib/chroot/clfs_0b_login_as_clfs_user_run_as_host_root.sh
@@ -1,24 +1,6 @@
 #!/bin/bash
 
-CLFS=/mnt/clfs
 CLFSUSER=clfs
-CLFSHOME=${CLFS}/home
-CLFSSOURCES=${CLFS}/sources
-CLFSTOOLS=${CLFS}/tools
-CLFSCROSSTOOLS=${CLFS}/cross-tools
-CLFSFILESYSTEM=ext4
-CLFSROOTDEV=/dev/sda4
-CLFSHOMEDEV=/dev/sda5
-
-export CLFS=/mnt/clfs
-export CLFSHOME=/mnt/clfs/home
-export FILESYSTEM=ext4
-export CLFSROOTDEV=/dev/sda4
-export CLFSHOMEDEV=/dev/sda5
-export CLFSSOURCES=/mnt/clfs/sources
-export CLFSTOOLS=/mnt/clfs/tools
-export CLFSCROSSTOOLS=/mnt/clfs/cross-tools
-export CLFSUSER=clfs
 
 passwd ${CLFSUSER}
 su - clfs
