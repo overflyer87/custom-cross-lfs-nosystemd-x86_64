3ee025420ee0ce3b5323d9cf4357988fb915060b
overflyer <puetz-thomas@gmx.net>
2017-08-06 20:14:10 +0200

    Update bclfs_5c_MATE_Desktop_x64.sh

--- clfs_sysvinit_20170702_x86_64_multilib/chroot/bclfs/bclfs_5c_MATE_Desktop_x64.sh
+++ clfs_sysvinit_20170702_x86_64_multilib/chroot/bclfs/bclfs_5c_MATE_Desktop_x64.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 function checkBuiltPackage() {
-
+echo " "
 echo "Did everything build fine?: [Y/N]"
 while read -n1 -r -p "[Y/N]   " && [[ $REPLY != q ]]; do
   case $REPLY in
@@ -12,7 +12,7 @@ while read -n1 -r -p "[Y/N]   " && [[ $REPLY != q ]]; do
     *) echo " Try again. Type y or n";;
   esac
 done
-
+echo " "
 }
 
 #Building the final CLFS System
@@ -1209,7 +1209,7 @@ CC="gcc ${BUILD64}" \
 PKG_CONFIG_PATH="${PKG_CONFIG_PATH64}" make LIBDIR=/usr/lib64 PREFIX=/usr
 sudo make LIBDIR=/usr/lib64 PREFIX=/usr install
 
-cd ${CLFSSOURCES}/bootscripts
+cd ${CLFSSOURCES}/blfs-bootscripts
 sudo make install-avahi
 
 cd ${CLFSSOURCES}/xc/mate
