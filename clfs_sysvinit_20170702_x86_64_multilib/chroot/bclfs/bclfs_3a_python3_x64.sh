44a5968a231c853b93846b72018b918ac2fdd022
overflyer <puetz-thomas@gmx.net>
2017-08-06 15:36:58 +0200

    Create bclfs_3a_python3_x64.sh

---
+++ clfs_sysvinit_20170702_x86_64_multilib/chroot/bclfs/bclfs_3a_python3_x64.sh
@@ -0,0 +1,139 @@
+#!/bin/bash
+
+function checkBuiltPackage() {
+
+echo "Did everything build fine?: [Y/N]"
+while read -n1 -r -p "[Y/N]   " && [[ $REPLY != q ]]; do
+  case $REPLY in
+    Y) break 1;;
+    N) echo "$EXIT"
+       echo "Fix it!"
+       exit 1;;
+    *) echo " Try again. Type y or n";;
+  esac
+done
+
+}
+
+function as_root()
+{
+  if   [ $EUID = 0 ];        then $*
+  elif [ -x /usr/bin/sudo ]; then sudo $*
+  else                            su -c \\"$*\\"
+  fi
+}
+
+export -f as_root
+
+function buildSingleXLib64() {
+ ./configure $XORG_CONFIG64
+  make
+  as_root make install
+}
+
+export -f buildSingleXLib64
+
+#Building the final CLFS System
+CLFS=/
+CLFSHOME=/home
+CLFSSOURCES=/sources
+CLFSTOOLS=/tools
+CLFSCROSSTOOLS=/cross-tools
+CLFSFILESYSTEM=ext4
+CLFSROOTDEV=/dev/sda4
+CLFSHOMEDEV=/dev/sda5
+MAKEFLAGS="-j$(nproc)"
+BUILD32="-m32"
+BUILD64="-m64"
+CLFS_TARGET32="i686-pc-linux-gnu"
+PKG_CONFIG_PATH=/usr/lib64/pkgconfig
+PKG_CONFIG_PATH64=/usr/lib64/pkgconfig
+ACLOCAL="aclocal -I $XORG_PREFIX/share/aclocal"
+
+export CLFS=/
+export CLFSUSER=clfs
+export CLFSHOME=/home
+export CLFSSOURCES=/sources
+export CLFSTOOLS=/tools
+export CLFSCROSSTOOLS=/cross-tools
+export CLFSFILESYSTEM=ext4
+export CLFSROOTDEV=/dev/sda4
+export CLFSHOMEDEV=/dev/sda5
+export MAKEFLAGS="-j$(nproc)"
+export BUILD32="-m32"
+export BUILD64="-m64"
+export CLFS_TARGET32="i686-pc-linux-gnu"
+export PKG_CONFIG_PATH=/usr/lib64/pkgconfig
+export PKG_CONFIG_PATH64=/usr/lib64/pkgconfig
+export ACLOCAL="aclocal -I $XORG_PREFIX/share/aclocal"
+
+cd ${CLFSSOURCES}
+
+#Expat (Needed by Python) 64-bit
+wget http://downloads.sourceforge.net/expat/expat-2.1.0.tar.gz -O \
+  expat-2.1.0.tar.gz
+  
+mkdir expat && tar xf expat-*.tar.* -C expat --strip-components 1
+cd expat
+
+USE_ARCH=64 PKG_CONFIG_PATH="${PKG_CONFIG_PATH64}"
+CC="gcc ${BUILD64}" CXX="g++ ${BUILD64}" ./configure \
+  --prefix=/usr \
+  --libdir=/usr/lib64 \
+  --disable-static \
+  --enable-shared &&
+  
+make LIBDIR=/usr/lib64 PREFIX=/usr 
+as_root make LIBDIR=/usr/lib64 PREFIX=/usr install
+  
+install -v -m755 -d /usr/share/doc/expat-2.1.0
+install -v -m644 doc/*.{html,png,css} /usr/share/doc/expat-2.1.0
+
+cd ${CLFSSOURCES}
+checkBuiltPackage
+rm -rf expat
+
+#Python 3 64-bit
+wget https://www.python.org/ftp/python/3.6.2/Python-3.6.2.tar.xz -O \
+  Python-3.6.2.tar.xz
+
+#link is down as of 2017-08-04 :(
+#wget http://pkgs.fedoraproject.org/cgit/rpms/python3.git/plain/00102-lib64.patch -O \
+#python360-multilib.patch
+  
+wget https://docs.python.org/3.6/archives/python-3.6.0-docs-html.tar.bz2 -O \
+  python-360-docs.tar.bz2
+  
+mkdir Python-3 && tar xf Python-3.6*.tar.xz -C Python-3 --strip-components 1
+cd Python-3
+
+patch -Np1 -i ../python360-multilib.patch
+
+USE_ARCH=64 CXX="/usr/bin/g++ ${BUILD64}" \
+    CC="/usr/bin/gcc ${BUILD64}" \
+    PKG_CONFIG_PATH="${PKG_CONFIG_PATH64}" ./configure \
+            --prefix=/usr       \
+            --enable-shared     \
+            --with-system-expat \
+            --with-system-ffi   \
+            --libdir=/usr/lib64 \
+            --with-custom-platlibdir=/usr/lib64 \
+            --with-ensurepip=yes &&
+
+make PREFIX=/usr LIBDIR=/usr/lib64 PLATLIBDIR=/usr/lib64 platlibdir=/usr/lib64 &&
+as_root make install PREFIX=/usr LIBDIR=/usr/lib64 PLATLIBDIR=/usr/lib64 \
+  platlibdir=/usr/lib64
+
+as_root chmod -v 755 /usr/lib64/libpython3.6m.so
+as_root chmod -v 755 /usr/lib64/libpython3.so
+
+sudo install -v -dm755 /usr/share/doc/python-3.6.0/html &&
+sudo tar --strip-components=1 \
+    --no-same-owner \
+    --no-same-permissions \
+    -C /usr/share/doc/python-3.6.0/html \
+    -xvf ../python-360-docs.tar.bz2
+
+cd ${CLFSSOURCES}
+checkBuiltPackage
+rm -rf Python-3
