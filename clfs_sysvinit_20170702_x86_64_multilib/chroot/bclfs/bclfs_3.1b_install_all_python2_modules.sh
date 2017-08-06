634b0cf80b65c223fffa30b655326fdb6d67fa29
overflyer <puetz-thomas@gmx.net>
2017-08-06 23:25:43 +0200

    Create bclfs_3.1b_install_all_python2_modules.sh

---
+++ clfs_sysvinit_20170702_x86_64_multilib/chroot/bclfs/bclfs_3.1b_install_all_python2_modules.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+function checkBuiltPackage() {
+echo " "
+echo "Did everything build fine?: [Y/N]"
+while read -n1 -r -p "[Y/N]   " && [[ $REPLY != q ]]; do
+  case $REPLY in
+    Y) break 1;;
+    N) echo "$EXIT"
+       echo "Fix it!"
+       exit 1;;
+    *) echo " Try again. Type y or n";;
+  esac
+done
+echo " "
+}
+
+#Building the final CLFS System
+CLFS=/
+CLFSHOME=/home
+CLFSSOURCES=/sources
+CLFSTOOLS=/tools
+CLFSCROSSTOOLS=/cross-tools
+CLFSFILESYSTEM=ext4
+CLFSROOTDEV=/dev/sda4
+CLFSHOMEDEV=/dev/sda5
+MAKEFLAGS="-j$(nproc)"
+BUILD32="-m32"
+BUILD64="-m64"
+CLFS_TARGET32="i686-pc-linux-gnu"
+PKG_CONFIG_PATH=/usr/lib64/pkgconfig
+PKG_CONFIG_PATH64=/usr/lib64/pkgconfig
+ACLOCAL="aclocal -I $XORG_PREFIX/share/aclocal"
+
+export CLFS=/
+export CLFSUSER=clfs
+export CLFSHOME=/home
+export CLFSSOURCES=/sources
+export CLFSTOOLS=/tools
+export CLFSCROSSTOOLS=/cross-tools
+export CLFSFILESYSTEM=ext4
+export CLFSROOTDEV=/dev/sda4
+export CLFSHOMEDEV=/dev/sda5
+export MAKEFLAGS="-j$(nproc)"
+export BUILD32="-m32"
+export BUILD64="-m64"
+export CLFS_TARGET32="i686-pc-linux-gnu"
+export PKG_CONFIG_PATH=/usr/lib64/pkgconfig
+export PKG_CONFIG_PATH64=/usr/lib64/pkgconfig
+export ACLOCAL="aclocal -I $XORG_PREFIX/share/aclocal"
+
+cd ${CLFSSOURCES}
