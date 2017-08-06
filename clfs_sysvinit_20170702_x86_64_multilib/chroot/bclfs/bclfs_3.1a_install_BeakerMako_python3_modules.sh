29b88753db6c4c664acf2ef4bfa2152d6c36141c
overflyer <puetz-thomas@gmx.net>
2017-08-06 23:31:51 +0200

    Update and rename bclfs_3.1a_install_all_python3_modules.sh to bclfs_3.1a_install_BeakerMako_python3_modules.sh

--- clfs_sysvinit_20170702_x86_64_multilib/chroot/bclfs/bclfs_3.1a_install_BeakerMako_python3_modules.sh
+++ clfs_sysvinit_20170702_x86_64_multilib/chroot/bclfs/bclfs_3.1a_install_BeakerMako_python3_modules.sh
@@ -50,3 +50,22 @@ export PKG_CONFIG_PATH64=/usr/lib64/pkgconfig
 export ACLOCAL="aclocal -I $XORG_PREFIX/share/aclocal"
 
 cd ${CLFSSOURCES}
+
+####EXAMPLE!!!!! Thats how it should be done for all modules
+
+#PyCrypto
+wget https://pypi.python.org/packages/60/db/645aa9af249f059cc3a368b118de33889219e0362141e75d4eaf6f80f163/pycrypto-2.6.1.tar.gz -O \
+	pycrypto-2.6.1.tar.gz
+
+mkdir pycrypto && tar xf pycrypto-*.tar.* -C pycrypto --strip-components 1
+cd pycrypto
+
+#sudo python2-64 setup.py build 
+#sudo python2-64 setup.py install --verbose --prefix=/usr/lib64 --install-lib=/usr/lib64/python2.7/site-packages --optimize=1
+sudo python3.6 setup.py build
+sudo python3.6 setup.py install --verbose --prefix=/usr/lib64 --install-lib=/usr/lib64/python3.6/site-packages --optimize=1
+
+cd ${CLFSSOURCES}/xc/mate
+checkBuiltPackage
+rm -rf pycrypto
+
