f7a70bbf69bf1c891f2d25d250344ccd31652e1f
overflyer <puetz-thomas@gmx.net>
2017-08-07 10:37:52 +0200

    Update bclfs_3.1b_install_PreXorg_BeakerMako_python2_modules.sh

--- clfs_sysvinit_20170702_x86_64_multilib/chroot/bclfs/bclfs_3.1b_install_PreXorg_BeakerMako_python2_modules.sh
+++ clfs_sysvinit_20170702_x86_64_multilib/chroot/bclfs/bclfs_3.1b_install_PreXorg_BeakerMako_python2_modules.sh
@@ -49,4 +49,58 @@ export PKG_CONFIG_PATH=/usr/lib64/pkgconfig
 export PKG_CONFIG_PATH64=/usr/lib64/pkgconfig
 export ACLOCAL="aclocal -I $XORG_PREFIX/share/aclocal"
 
-cd ${CLFSSOURCES}
+cd ${CLFSSOURCES}/xc
+
+#Build Python Module Beaker
+#required by Python Module Mako
+
+wget https://pypi.python.org/packages/93/b2/12de6937b06e9615dbb3cb3a1c9af17f133f435bdef59f4ad42032b6eb49/Beaker-1.9.0.tar.gz -O \
+  Beaker-1.9.0.tar.gz
+
+mkdir pybeaker && tar xf Beaker-*.tar.* -C pybeaker --strip-components 1
+cd pybeaker
+
+CXX="g++ ${BUILD64}" USE_ARCH=64 CC="gcc ${BUILD64}" PKG_CONFIG_PATH="${PKG_CONFIG_PATH64}" 
+
+as_root python2.7 setup.py build
+as_root python2.7 setup.py install --verbose --prefix=/usr/lib64 --install-lib=/usr/lib64/python2.7/site-packages --optimize=1
+
+cd ${CLFSSOURCES}/xc
+checkBuiltPackage
+rm -rf pybeaker
+
+#Build Python Module MarkupSafe
+#required by Python Module Mako
+
+wget https://files.pythonhosted.org/packages/4d/de/32d741db316d8fdb7680822dd37001ef7a448255de9699ab4bfcbdf4172b/MarkupSafe-1.0.tar.gz -O \
+  MarkupSafe-1.0.tar.gz
+
+mkdir pyMarkupSafe && tar xf MarkupSafe-*.tar.* -C pyMarkupSafe --strip-components 1
+cd pyMarkupSafe
+
+CXX="g++ ${BUILD64}" USE_ARCH=64 CC="gcc ${BUILD64}" PKG_CONFIG_PATH="${PKG_CONFIG_PATH64}" 
+
+as_root python2.7 setup.py build
+as_root python2.7 setup.py install --verbose --prefix=/usr/lib64 --install-lib=/usr/lib64/python2.7/site-packages --optimize=1
+
+cd ${CLFSSOURCES}/xc
+checkBuiltPackage
+rm -rf pyMarkupSafe
+
+#Python 3.6 Mako modules
+#64-bit
+wget https://pypi.python.org/packages/source/M/Mako/Mako-1.0.4.tar.gz -O \
+  Mako-1.0.4.tar.gz
+
+mkdir pymako && tar xf Mako-*.tar.* -C pymako --strip-components 1
+cd pymako
+
+CXX="g++ ${BUILD64}" USE_ARCH=64 CC="gcc ${BUILD64}" PKG_CONFIG_PATH="${PKG_CONFIG_PATH64}" 
+as_root python2.7 setup.py build
+as_root python2.7 setup.py install --verbose --prefix=/usr/lib64 --install-lib=/usr/lib64/python2.7/site-packages --optimize=1
+
+cd ${CLFSSOURCES}/xc
+checkBuiltPackage
+rm -rf pymako
+
+
