7bd02776d52880ade31b020778f644eece8b1488
overflyer <puetz-thomas@gmx.net>
2017-08-13 15:05:33 +0200

    Update clfs_6d_final_system_RASRC.sh

--- clfs_sysvinit_20170702_x86_64_multilib/chroot/clfs_6d_final_system_RASRC.sh
+++ clfs_sysvinit_20170702_x86_64_multilib/chroot/clfs_6d_final_system_RASRC.sh
@@ -1003,7 +1003,7 @@ rm -rf nano
 
 
 echo " "
-echo "Next execute script #6d for UEFI boot. Legacy boot option will maybe never follow. Low priority!"
+echo "Next execute script #7.1 for UEFI boot. Legacy boot option will maybe never follow. Low priority!"
 echo " "
 
 exit
