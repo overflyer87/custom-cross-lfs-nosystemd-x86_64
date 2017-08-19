73b52bcf6f250c32f8e00943eb7b2312c3b60cb2
overflyer <puetz-thomas@gmx.net>
2017-08-19 10:30:59 +0200

    Update clfs_7.1_final_system_boot_uefi_RASRC.sh

--- clfs_sysvinit_20170702_x86_64_multilib/chroot/clfs_7.1_final_system_boot_uefi_RASRC.sh
+++ clfs_sysvinit_20170702_x86_64_multilib/chroot/clfs_7.1_final_system_boot_uefi_RASRC.sh
@@ -446,7 +446,7 @@ fs_uuid=$(blkid -o value -s PARTUUID /dev/sda4)
 
 cat > /boot/efi/loader/entries/clfs-uefi.conf << "EOF"
 title   Cross Linux from Scratch
-linux   /vmlinuz-clfs-4.12.6
+linux   /vmlinuz-clfs-4.12.8
 initrd  /intel-ucode.img
 EOF
 
