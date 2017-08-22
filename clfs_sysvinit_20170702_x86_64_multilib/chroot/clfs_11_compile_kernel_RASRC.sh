690e5cd5f7293042d958913a736ffcd70f834cdc
overflyer <puetz-thomas@gmx.net>
2017-08-22 22:15:24 +0200

    Update clfs_11_compile_kernel_RASRC.sh

--- clfs_sysvinit_20170702_x86_64_multilib/chroot/clfs_11_compile_kernel_RASRC.sh
+++ clfs_sysvinit_20170702_x86_64_multilib/chroot/clfs_11_compile_kernel_RASRC.sh
@@ -36,6 +36,9 @@ cd ${CLFSSOURCES}
 mkdir linux && tar xf linux-*.tar.* -C linux --strip-components 1
 cd linux
 
+rm -rf /lib/modules/4.12.8*
+rm -rf /lib/modules/*4.12.8*
+
 make mrproper
 cp ${CLFSSOURCES}/kernel.conf ${CLFSSOURCES}/linux/.config
 
