d46b2b33bca20acf74720b495ea718f36745e7cb
overflyer <puetz-thomas@gmx.net>
2017-08-23 23:06:58 +0200

    Create bclfs_DE2_XFCE1_x64_RASUN.sh

---
+++ clfs_sysvinit_20170702_x86_64_multilib/chroot/bclfs/DEs/XFCE/bclfs_DE2_XFCE1_x64_RASUN.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+function checkBuiltPackage() {
+echo " "
+echo "Make sure you are able to continue... [Y/N]"
+while read -n1 -r -p "[Y/N]   " && [[ $REPLY != q ]]; do
+  case $REPLY in
+    Y) break 1;;
+    N) echo "$EXIT"
+       echo "Fix it!"
+       exit 1;;
+    *) echo " Try again. Type y or n";;
+  esac
+done
+echo " "
+}
+
+#Building the final CLFS System
+CLFS=/
+CLFSHOME=/home
+CLFSSOURCES=/sources
+CLFSTOOLS=/tools
+CLFSCROSSTOOLS=/cross-tools
+CLFSFILESYSTEM=ext4
+CLFSROOTDEV=/dev/sda4
+CLFSHOMEDEV=/dev/sda5
+MAKEFLAGS="-j$(nproc)"
+BUILD32="-m32"
+BUILD64="-m64"
+CLFS_TARGET32="i686-pc-linux-gnu"
+PKG_CONFIG_PATH=/usr/lib64/pkgconfig
+PKG_CONFIG_PATH64=/usr/lib64/pkgconfig
+
+export CLFS=/
+export CLFSUSER=clfs
+export CLFSHOME=/home
+export CLFSSOURCES=/sources
+export CLFSTOOLS=/tools
+export CLFSCROSSTOOLS=/cross-tools
+export CLFSFILESYSTEM=ext4
+export CLFSROOTDEV=/dev/sda4
+export CLFSHOMEDEV=/dev/sda5
+export MAKEFLAGS="-j$(nproc)"
+export BUILD32="-m32"
+export BUILD64="-m64"
+export CLFS_TARGET32="i686-pc-linux-gnu"
+export PKG_CONFIG_PATH=/usr/lib64/pkgconfig
+export PKG_CONFIG_PATH64=/usr/lib64/pkgconfig
+
+cd ${CLFSSOURCES}
+cd ${CLFSSOURCES}/xc/mate
+
+#We will only do 64-bit builds in this script
+#We compiled Xorg with 32-bit libraries
+#That should suffice
+
+PKG_CONFIG_PATH="${PKG_CONFIG_PATH64}" 
+USE_ARCH=64 
+CXX="g++ ${BUILD64}" 
+CC="gcc ${BUILD64}"
+
+export PKG_CONFIG_PATH="${PKG_CONFIG_PATH64}" 
+export USE_ARCH=64 
+export CXX="g++ ${BUILD64}" 
+export CC="gcc ${BUILD64}"
+
+#gtksourceview3
+wget http://ftp.gnome.org/pub/gnome/sources/gtksourceview/3.24/gtksourceview-3.24.3.tar.xz -O \
+    gtksourceview-3.24.3.tar.xz
+
+mkdir gtksourceview && tar xf gtksourceview-*.tar.* -C gtksourceview --strip-components 1
+cd gtksourceview
+
+CC="gcc ${BUILD64}" CXX="g++ ${BUILD64}" \
+USE_ARCH=64 PKG_CONFIG_PATH=${PKG_CONFIG_PATH64} ./configure --prefix=/usr\
+    --libdir=/usr/lib64 \
+    --disable-gtk-doc
+    
+PKG_CONFIG_PATH=${PKG_CONFIG_PATH64} CC="gcc ${BUILD64}" USE_ARCH=64 \
+CXX="g++ ${BUILD64}" make PREFIX=/usr LIBDIR=/usr/lib64
+
+sudo make PREFIX=/usr LIBDIR=/usr/lib64 install
+
+cd ${CLFSSOURCES}/xc/mate
+checkBuiltPackage
+rm -rf gtksourceview
