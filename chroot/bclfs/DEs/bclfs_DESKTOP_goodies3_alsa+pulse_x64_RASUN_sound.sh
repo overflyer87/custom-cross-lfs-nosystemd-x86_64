da89d0b08a8d8ef56544d1e9d514c769bc353007
overflyer <overflyer87@gmx.net>
2017-10-14 14:53:56 +0200

    Update and rename bclfs_DESKTOP_goodies3_x64_RASUN_sound.sh to bclfs_DESKTOP_goodies3_alsa+pulse_x64_RASUN_sound.sh

--- chroot/bclfs/DEs/bclfs_DESKTOP_goodies3_alsa+pulse_x64_RASUN_sound.sh
+++ chroot/bclfs/DEs/bclfs_DESKTOP_goodies3_alsa+pulse_x64_RASUN_sound.sh
@@ -17,13 +17,7 @@ echo ""
 
 #Building the final CLFS System
 CLFS=/
-CLFSHOME=/home
 CLFSSOURCES=/sources
-CLFSTOOLS=/tools
-CLFSCROSSTOOLS=/cross-tools
-CLFSFILESYSTEM=ext4
-CLFSROOTDEV=/dev/sda4
-CLFSHOMEDEV=/dev/sda5
 MAKEFLAGS="-j$(nproc)"
 BUILD32="-m32"
 BUILD64="-m64"
@@ -32,14 +26,7 @@ PKG_CONFIG_PATH=/usr/lib64/pkgconfig
 PKG_CONFIG_PATH64=/usr/lib64/pkgconfig
 
 export CLFS=/
-export CLFSUSER=clfs
-export CLFSHOME=/home
 export CLFSSOURCES=/sources
-export CLFSTOOLS=/tools
-export CLFSCROSSTOOLS=/cross-tools
-export CLFSFILESYSTEM=ext4
-export CLFSROOTDEV=/dev/sda4
-export CLFSHOMEDEV=/dev/sda5
 export MAKEFLAGS="-j$(nproc)"
 export BUILD32="-m32"
 export BUILD64="-m64"
@@ -48,6 +35,7 @@ export PKG_CONFIG_PATH=/usr/lib64/pkgconfig
 export PKG_CONFIG_PATH64=/usr/lib64/pkgconfig
 
 cd ${CLFSSOURCES}
+mkdir -pv cd ${CLFSSOURCES}/xc/mate
 cd ${CLFSSOURCES}/xc/mate
 
 #We will only do 64-bit builds in this script
