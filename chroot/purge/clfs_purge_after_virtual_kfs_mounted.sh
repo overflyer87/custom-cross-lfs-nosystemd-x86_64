a899130ce578b77855098647e34c499c080903e5
overflyer <overflyer87@gmx.net>
2017-11-05 18:56:11 +0100

    Update clfs_purge_after_virtual_kfs_mounted.sh

--- chroot/purge/clfs_purge_after_virtual_kfs_mounted.sh
+++ chroot/purge/clfs_purge_after_virtual_kfs_mounted.sh
@@ -1,20 +1,20 @@
 
 #!/bin/bash
 
-#Reverse CLFS Installation until Temporary System
+#Purge CLFS if you already used clfs_4 script to chroot into temp system
 
 CLFS=/mnt/clfs
 CLFSHOME=/mnt/clfs/home
 CLFSUSER="clfs"
 
-sudo -f umount ${CLFS}/dev/pts
-sudo -f umount ${CLFS}/dev
-sudo -f umount ${CLFS}/home
-sudo -f umount ${CLFS}/boot/efi
-sudo -f umount ${CLFS}/sys/firmware/efi/efivars
-sudo -f umount ${CLFS}/sys
-sudo -f umount ${CLFS}/run
-sudo -f umount ${CLFS}/proc
+sudo umount -f ${CLFS}/dev/pts
+sudo umount -f ${CLFS}/dev
+sudo umount -f ${CLFSHOME}
+sudo umount -f ${CLFS}/boot/efi
+sudo umount -f ${CLFS}/sys/firmware/efi/efivars
+sudo umount -f ${CLFS}/sys
+sudo umount -f ${CLFS}/run
+sudo umount -f ${CLFS}/proc
 sudo userdel ${CLFSUSER}
 sudo groupdel ${CLFSUSER}
 sudo rm -rf /home/${CLFSUSER}
