0b77be064cbbe8877b004a4367ec39338c6225cb
overflyer <overflyer87@gmx.net>
2017-11-05 18:46:46 +0100

    Update clfs_purge_after_virtual_kfs_mounted.sh

--- chroot/purge/clfs_purge_after_virtual_kfs_mounted.sh
+++ chroot/purge/clfs_purge_after_virtual_kfs_mounted.sh
@@ -10,14 +10,16 @@ CLFSUSER="clfs"
 sudo -f umount ${CLFS}/dev/pts
 sudo -f umount ${CLFS}/dev
 sudo -f umount ${CLFS}/home
+sudo -f umount ${CLFS}/boot/efi
 sudo -f umount ${CLFS}/sys/firmware/efi/efivars
 sudo -f umount ${CLFS}/sys
 sudo -f umount ${CLFS}/run
 sudo -f umount ${CLFS}/proc
 sudo userdel ${CLFSUSER}
 sudo groupdel ${CLFSUSER}
-sudo rm -r /home/${CLFSUSER}
-sudo rm -rf ${CLFS}/*
-sudo umount -f ${CLFS}
+sudo rm -rf /home/${CLFSUSER}
 sudo unlink /cross-tools 
 sudo unlink /tools
+sudo rm -rf ${CLFS}/*
+sudo umount -f ${CLFS}
+
